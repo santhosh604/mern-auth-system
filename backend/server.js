1e53764a620daf0fc313bef85767474fdf5baab9
Santhosh <santhoshchinnolla75@gmail.com>
2026-03-25 23:09:23 +0530

    testing

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
 
 const app = express();
-app.set("trust proxy", 1);
+app.set("trust proxy", 1)
 app.use(express.json());
 app.use(cors({origin: "https://mern-auth-system-hazel.vercel.app", credentials: true}));
 app.use(cookieParser());
@@ -17,4 +17,4 @@ app.use("/auth", authRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running perfect`)
-})
+});
